e6ef55479bfc20ff6d8b1b400e9c0a6c62cb4a1c
Nas-Islam <74771100+Nas-Islam@users.noreply.github.com>
2021-01-02 23:03:43 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,10 +6,4 @@ sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-
-sudo mkdir /opt/socialgamingproject
-sudo chown -R jenkins /opt/socialgamingproject
-
-sudo systemctl daemon-reload
-sudo systemctl stop app.service
-sudo systemctl start app.service
+python3 app.py
