451eab2b5c26952fef88693a036e8ebe4bcf527f
Nas-Islam <74771100+Nas-Islam@users.noreply.github.com>
2021-01-03 14:54:14 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
-#!/bin/bash 
-sudo python3 -m venv venv
+#!/bin/bash
+sudo apt update
+sudo apt-get install python3-venv
+
+python3 -m venv venv
 source venv/bin/activate
+pip3 install -r requirements.txt
+pip3 install flask-wtf
 python3 app.py
