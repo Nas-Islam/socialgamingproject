598716d0d27117a9bbe54a42dc092e2d41df8f7e
Nas-Islam <74771100+Nas-Islam@users.noreply.github.com>
2021-01-03 00:15:27 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash 
-cd /opt/socialgamingproject
-sudo mkdir /socialgamingproject
-sudo chown -R jenkins /socialgamingproject
 sudo python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
 python3 app.py
