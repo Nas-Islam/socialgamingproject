cc2092b5bdcb21e06f679c0dc4edf6b0e6235e76
Nas-Islam <74771100+Nas-Islam@users.noreply.github.com>
2021-01-03 00:02:09 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 cd /opt/socialgamingproject
 sudo mkdir /socialgamingproject
-sudo chown -R /socialgamingproject
+sudo chown -R jenkins /socialgamingproject
 sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
