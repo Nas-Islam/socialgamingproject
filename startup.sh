cbdc84f6be172a679b2cec2e5ec9a36fcb8921be
Nas-Islam <74771100+Nas-Islam@users.noreply.github.com>
2021-01-04 10:56:15 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,13 +8,10 @@ pip3 install -r requirements.txt
 #pip3 install flask-wtf
 pytest
 
-
-python3 create.py
-python3 app.py
-
-
 sudo systemctl daemon-reload
 sudo systemctl stop app.service
 sudo systemctl start app.service
 
+python3 create.py
+python3 app.py
 
