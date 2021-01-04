53312e1eb7e55640d64fda001097d018a5a22bd4
Nas-Islam <islam626@hotmail.co.uk>
2021-01-04 23:38:20 +0000

    Adapting startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-sudo apt update
-sudo apt-get install python3-venv -y
-
-python3 -m venv venv
+cd /opt/sgproject
+sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-
-pytest --cov=application --cov-report term-missing
-
-sudo systemctl daemon-reload
-sudo systemctl stop app.service
-sudo systemctl start app.service
-
 python3 create.py
 python3 app.py
 
