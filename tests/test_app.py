efdeae39b5296d285a2af61086f5142fb414c9a2
Nas-Islam <islam626@hotmail.co.uk>
2021-01-04 14:45:38 +0000

    Made changes to pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,10 +20,9 @@ class TestBase(TestCase):
         db.create_all()
 
         samplegame1 = Game(name="Test Game", platform = 1, genre = "Test Genre")
-        samplegame2 = Game(name="Test Game2", platform = 2, genre = "Test Genre2")
         db.session.add(samplegame1)
-        db.session.add(samplegame1)
-       # db.session.commit()
+        db.session.commit()
+        
         samplerating1 = Rating(name=samplegame1.name, rating=1, review="Test Review", username="Test_User")
         db.session.add(samplerating1)
         db.session.commit()
@@ -31,7 +30,8 @@ class TestBase(TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-    
+
+## Tests that each webpage can be viewed without any errors.
 class TestViews(TestBase):
     def test_home_get(self):
         response = self.client.get(url_for('home'))
@@ -73,7 +73,7 @@ class TestViews(TestBase):
         response = self.client.get(url_for('deletegame', id=1), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-## Tests all the features of the task table are viewed on the homepage  
+## Tests all the features are viewed on the homepage & gamepage 
 class TestRead(TestBase):
     def test_read_home(self):
         response = self.client.get(url_for("home"))
@@ -83,7 +83,7 @@ class TestRead(TestBase):
         response = self.client.get(url_for('gamepage'))
         self.assertIn(b"Test Game", response.data)
 
-## Tests the name and description is added from the form and that the changes can be viewed on the homepage
+## Tests the rating and game is added from the form and that the changes can be viewed on the gamepage & review page.
 class TestAdd(TestBase):
     def test_add_rating(self):
         response = self.client.post(
@@ -104,7 +104,9 @@ class TestAdd(TestBase):
         self.assertIn(b"1", response.data)
         self.assertIn(b"Test Genre", response.data)
 
-    def test_add_search(self):
+## Tests the search form can find the selected reviews and games.
+class TestSearch(TestBase):
+    def test_search(self):
         response = self.client.post(
             url_for('searchrating'),
             data = dict(game_name = "Test Game"),
@@ -112,6 +114,7 @@ class TestAdd(TestBase):
         )
         self.assertIn(b"Test Game", response.data)
 
+## Tests that the user can update the selected review.
 class TestUpdate(TestBase):
     def test_update_review(self):
         response = self.client.post(
@@ -122,15 +125,8 @@ class TestUpdate(TestBase):
         self.assertIn(b"Changed review", response.data)
         self.assertIn(b"2", response.data)
 
-## Tests the selected data is deleted from the table and is no longer viewable on the homepage
+## Tests the selected data is deleted from the table and is no longer viewable on the game and review page.
 class TestDelete(TestBase):
-    def test_delete_game(self):
-        response = self.client.post(
-            url_for('deletegame', id=2),
-            follow_redirects=True
-        )
-        self.assertNotIn(b"Test Game2", response.data)
-
     def test_delete_review(self):
         response = self.client.post(
             url_for('delete', id=1),
@@ -138,3 +134,9 @@ class TestDelete(TestBase):
         )
         self.assertNotIn(b"Test Review", response.data)
 
+    def test_delete_game(self):
+        response = self.client.post(
+            url_for('deletegame', id=1),
+            follow_redirects=True
+        )
+        self.assertNotIn(b"Test Game", response.data)
